91c24d27fb417c148eebb6846c35dcccee6efb17
Oleg Kirillov <kirolegg@gmail.com>
2018-11-18 21:14:04 +0300

    test_script

---
+++ bot.py
@@ -0,0 +1,311 @@
+#TODO:
+#1. M3U Playlist (Radio bot)
+#2. Youtube playlist
+#3. Rewrite utils
+#4. Use contex comands
+#5. Various comand (Random, etc)
+#6. Steam api
+import sys
+import re
+import configparser
+import urllib.parse
+import urllib.request
+import discord
+from discord.ext import commands
+from utils import *
+import asyncio
+from subprocess import Popen
+import random
+import numpy
+import sounddevice as sd
+import queue
+
+
+
+class Bot:
+    client = None
+    voice_client = None
+    text_channel = None
+    player = None
+    player_yt = None
+    p_dl = None
+    speak = False
+    tcp_speak = False
+    curr_song = None
+    music_data = None
+    socket_stream = None
+    pcm_stream = None
+
+    def __init__(self, dc):
+        self.client = dc
+
+
+bot = Bot(discord.Client())
+
+if not discord.opus.is_loaded():
+    discord.opus.load_opus('opus')
+
+q_file = "queue.dat"
+yt_url_pattern = re.compile("^http(s)?:/\/(?:www\.)?youtube.com\/watch\?(?=.*v=\w+)(?:\S+)?$")
+server_id = '224886395427094528'
+token = 'NDMxOTc0NDkzMDc0NzUxNDg4.DamjLA.UuoFMdsXMuS9qh45zIDms64r9jk'
+admin_role = 'nazi'
+remove_file(q_file)
+
+sd.default.device = 3  # change to desired device
+sd.default.channels = 2
+sd.default.dtype = 'int16'
+sd.default.latency = 'low'
+sd.default.samplerate = 48000
+
+
+async def handle_echo(reader, writer):
+    if bot.tcp_speak:
+        data = await reader.read(3840)
+        message = data
+        addr = writer.get_extra_info('peername')
+        print("Received %r from %r" % (message, addr))
+
+        bot.music_data = data
+
+
+loop = asyncio.get_event_loop()
+coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
+server = loop.run_until_complete(coro)
+
+
+class PCMStream:
+    def __init__(self):
+        self.stream = sd.InputStream()
+        self.stream.start()
+
+    def read(self, num_bytes):
+        # frame is 4 bytes
+        frames = int(num_bytes / 4)
+        data = self.stream.read(frames)[0]
+        return data.tobytes()
+
+
+class Stream:
+    bot_ = None
+    def __init__(self, bot):
+        #self.stream = sd.InputStream()
+        #self.stream.start()
+        self.bot_ = bot
+
+    def read(self, num_bytes):
+        if self.bot_.music_data is None:
+            return b'0'*3840
+        return self.bot_.music_data
+
+
+
+
+	
+
+
+@bot.client.event
+async def on_message(message):
+    msg = []
+    if message.content.startswith('@@'):
+        msg = message.content.split()
+        try:
+            if 'youtu' in msg[1] and not bot.speak:
+                if bot.player:
+                    song_url = ''
+                    if yt_url_pattern.match(msg[1]):
+                        song_url = msg[1]
+                        try:
+                            this_song = fetch_song(song_url)
+                        except Exception as e:
+                            return
+                        # player.change_voice(bot.client.voice_client_in(bot.client.get_server('224886395427094528')))
+                        bot.player.queue(this_song)
+            elif 'join' in [msg[1]]:
+                if bot.speak or bot.tcp_speak:
+                    try:
+                        bot.player.stop()
+                        if bot.client.is_voice_connected(bot.client.get_server(server_id)):
+                            bot.voice_client.disconnect()
+                        if bot.voice_client:
+                            del bot.voice_client
+                        if bot.p_dl:
+                            del bot.p_dl
+                        if bot.player:
+                            del bot.player
+                    except Exception as e:
+                        return
+
+                if not bot.client.is_voice_connected(bot.client.get_server(server_id)):
+                    await bot.client.join_voice_channel(bot.client.get_channel(message.author.voice.voice_channel.id))
+                else:
+                    await bot.client.move_member(list(filter(lambda x: x.id == bot.client.user.id,
+                                                             bot.client.get_all_members()))[0],
+                                                 bot.client.get_channel(message.author.voice.voice_channel.id))
+                if not bot.voice_client or bot.speak or bot.tcp_speak:
+
+                    bot.voice_client = bot.client.voice_client_in(bot.client.get_server(server_id))
+                    bot.player = Player(bot.client, voice_client=bot.voice_client)
+                    bot.p_dl = Popen(["python", "yt_downloader.py"])
+                else:
+                    voice_client = bot.client.voice_client_in(bot.client.get_server(server_id))
+                    bot.player.change_voice(voice_client)
+                bot.speak = False
+                bot.tcp_speak = False
+            elif 'skip' in [msg[1]]:
+                if bot.player and not bot.speak:
+                    bot.player.skip()
+            elif 'volume' in [msg[1]]:
+                if bot.player:
+                    if bot.player.get_playlist():
+                        try:
+                            float(msg[2])
+                            bot.player.change_volume(float(msg[2]))
+                        except Exception as e:
+                            print("Not a float")
+            # elif 'душитель' in [msg[1].lower()]:
+            #     await bot.client.send_message(message.channel,
+            #                                   'Душитель: {0}'.format(random.choice(list(
+            #                                       filter(lambda x: x.id != bot.client.user.id,
+            #                                              bot.client.get_all_members()))).name))
+            elif 'purge' in [msg[1]]:
+                if len(list(filter(lambda x: x.name == admin_role, message.author.roles))) > 0:
+                    try:
+                        int(msg[2])
+                        await bot.client.purge_from(message.channel, limit=int(msg[2]))
+                    except Exception as e:
+                        print('Wrong value')
+            # elif 'test' in [msg[1]]:
+            #     bot.player.check_vars()
+            # elif 'rgg' in [msg[1]]:
+            #     try:
+            #         with open('rgg\\' + msg[2] + '.txt', 'r') as f:
+            #             await bot.client.send_message(message.channel,
+            #                                           'Играй в {0} на {1}'.format(random.choice(f.readlines()).rstrip(),
+            #                                                                       msg[2]))
+            #     except Exception as e:
+            #         print(e)
+            elif 'speak' in [msg[1]]:
+                if not bot.speak:
+                    try:
+                        if bot.tcp_speak:
+                            bot.tcp_speak = False
+                        if bot.player:
+                            bot.player.stop()
+                        if bot.client.is_voice_connected(bot.client.get_server(server_id)):
+                            bot.voice_client.disconnect()
+                        if bot.voice_client:
+                            del bot.voice_client
+                        if bot.p_dl:
+                            del bot.p_dl
+                        if bot.player:
+                            del bot.player
+                        remove_file(q_file)
+                    except Exception as e:
+                        return
+                if not bot.client.is_voice_connected(bot.client.get_server(server_id)):
+                    await bot.client.join_voice_channel(bot.client.get_channel(message.author.voice.voice_channel.id))
+                else:
+                    await bot.client.move_member(
+                        list(filter(lambda x: x.id == bot.client.user.id, bot.client.get_all_members()))[0],
+                        bot.client.get_channel(message.author.voice.voice_channel.id))
+                bot.voice_client = bot.client.voice_client_in(bot.client.get_server(server_id))
+                bot.player = bot.voice_client.create_stream_player(PCMStream())
+                bot.player.start()
+                bot.speak = True
+            elif 'speak_tcp' in [msg[1]]:
+                print('sssss')
+                if not bot.tcp_speak:
+                    try:
+                        if bot.speak:
+                            bot.speak = False
+                        if bot.player:
+                            bot.player.stop()
+                        if bot.client.is_voice_connected(bot.client.get_server(server_id)):
+                            bot.voice_client.disconnect()
+                        if bot.voice_client:
+                            del bot.voice_client
+                        if bot.p_dl:
+                            del bot.p_dl
+                        if bot.player:
+                            del bot.player
+                        remove_file(q_file)
+                    except Exception as e:
+                        print(e)
+                        return
+                if not bot.client.is_voice_connected(bot.client.get_server(server_id)):
+                    await bot.client.join_voice_channel(bot.client.get_channel(message.author.voice.voice_channel.id))
+                else:
+                    await bot.client.move_member(
+                        list(filter(lambda x: x.id == bot.client.user.id, bot.client.get_all_members()))[0],
+                        bot.client.get_channel(message.author.voice.voice_channel.id))
+                bot.voice_client = bot.client.voice_client_in(bot.client.get_server(server_id))
+                bot.player = bot.voice_client.create_stream_player(Stream(bot))
+                bot.player.start()
+                bot.tcp_speak = True
+            elif 'help' in [msg[1]]:
+                try:
+                    await bot.client.send_message(message.channel,
+                                                  '''
+--------------------------------------------------------
+|@@ join                    | Add bot to voice channel |
+|@@ [link to youtube video] | Play music from youtube  |
+|@@ skip                    | Skip youtube track       | 
+|@@ volume [from 0 to 1]    | Change bot volume        |
+|@@ purge [value]           | Delete [value] numbers of lines in chat |
+|@@ speak                   | Bot speak from line-in   |
+|@@ speak_tcp               | Bot speak from tcp       | 
+--------------------------------------------------------
+                                                  ''')
+                except Exception as e:
+                    print(e)
+
+				
+        except Exception as e:
+            print(e)
+
+
+# @bot.client.event
+async def background_loop():
+    await bot.client.wait_until_ready()
+    #ch = bot.client.get_channel('368512789549023243')
+   # await bot.client.change_presence(game=discord.Game(name=''))
+    # while not bot.client.is_closed:
+    #     for Member in bot.client.get_all_members():
+    #         Roles = Member.roles
+    #         if len(list(filter(lambda x: x.name == 'DOG', Roles))) > 0:
+    #             if not bot.client.is_voice_connected(bot.client.get_server(server_id)):
+    #                 await bot.client.join_voice_channel(ch)
+    #             await bot.client.move_member(Member, ch)
+    #     try:
+    #         if bot.player and not bot.speak:
+    #             if bot.player.get_playlist():
+    #                 if bot.curr_song != bot.player.get_playlist()[0]:
+    #                     bot.curr_song = bot.player.get_playlist()[0]
+    #                     await bot.client.change_presence(game=discord.Game(name=bot.curr_song.title))
+    #             else:
+    #                 await bot.client.change_presence(game=discord.Game(name=''))
+    #     except Exception as e:
+    #         print(e)
+    #
+    #     await asyncio.sleep(1)
+    while not bot.client.is_closed:
+        await asyncio.sleep(1)
+
+
+
+@bot.client.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.client.user.name)
+    print(bot.client.user.id)
+    print('------')
+
+
+bot.client.loop.create_task(background_loop())
+bot.client.run(token)
+loop.run_forever()
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
+
---
+++ utils.py
@@ -0,0 +1,243 @@
+import os
+import pickle
+import threading
+import time
+
+import asyncio
+import youtube_dl
+from concurrent.futures import ProcessPoolExecutor
+import inspect
+
+# Song model
+class Song:
+    def __init__(self, id, title, thumbnail, description, duration, views, likes, dislikes, playlist, uploader, url, source, requester=None,
+                 file=None):
+        self.id = id
+        self.title = title
+        self.thumbnail = thumbnail
+        self.description = description
+        self.duration = duration
+        # Create duration string
+        m, s = divmod(duration, 60)
+        h, m = divmod(m, 60)
+        duration_string = ("%d:%02d:%02d" % (h, m, s)) if h else ("%02d:%02d" % (m, s))
+        self.duration_string = duration_string
+        # -----------
+        self.views = views
+        self.likes = likes
+        self.dislikes = dislikes
+        self.playlist = playlist
+        self.requester = requester
+        self.uploader = uploader
+        self.url = url
+        self.source = source
+        self.file = file
+
+
+# Some file functions
+def remove_file(file):
+    if file:
+        if os.path.exists(file):
+            tries = 0
+            while tries < 5:
+                time.sleep(2)
+                tries += 1
+                try:
+                    os.remove(file)
+                    return 1
+                except OSError as e:
+                    print("Could not delete, retrying... " + e.strerror)
+
+
+def load_queue_file():
+    # check for queue file
+    if os.path.isfile("queue.dat"):
+        try:
+            with open("queue.dat", "rb") as f:
+                return pickle.load(f)
+        except EOFError:
+            print("EOFError: File seems to be empty...")
+            return []
+    else:
+        print('No old queue data found, starting with new queue.')
+        return update_queue_file([])
+
+
+def update_queue_file(playlist):
+    with open("queue.dat", "wb") as f:
+        pickle.dump(playlist, f)
+        return playlist
+
+
+# The music player
+class Player(object):
+    def __init__(self, bot, voice_client):
+        self.voice_client = voice_client
+        self.media_player = None
+        self.is_playing = False
+        self.timeout = 0
+        self.bot = bot
+        self.playlist = load_queue_file()
+        self.volume = 0.05
+        self.thread = None
+        self.is_alive = True
+
+        # start this player class as a new thread
+        self.thread = threading.Thread(target=self.run, args=())
+        self.thread.daemon = True  # Daemonize thread
+        self.thread.start()  # Start the execution
+
+    def play(self):
+        self.playlist = self.get_playlist()
+        if self.playlist:
+            if self.voice_client:
+                if self.playlist[0].file:
+                    if not self.is_playing:
+                        self.media_player = self.voice_client.create_ffmpeg_player(self.playlist[0].file, after=self.on_song_finished)
+                        self.media_player.volume = self.volume
+                        self.timeout = 0
+                        self.is_playing = True
+                        self.media_player.start()
+                else:
+                    # Wait max 30sec for the download, then skip it
+                    if self.timeout >= 30:
+                        print("Skipping song, took too long to download...")
+                        self.timeout = 0
+                        self.on_song_finished()
+                    else:
+                        if self.timeout == 0:
+                            print('\nWaiting for song to download... ')
+                        self.timeout += 1
+            else:
+                print("Voice client not ready yet")
+
+    def on_song_finished(self):
+        print('Song finished')
+        self.playlist = self.get_playlist()
+        # if not self.file_needed(self.playlist[0].file):
+        #     executor = ProcessPoolExecutor(1)
+        #     del_future = self.bot.loop.run_in_executor(executor, remove_file, self.playlist[0].file)
+        #     del_task = asyncio.ensure_future(del_future)
+        #     asyncio.ensure_future(del_task)
+        del self.playlist[0]
+        self.is_playing = False
+        update_queue_file(self.playlist)
+        self.play()
+
+    def skip(self):
+        print('Skipping song')
+        self.media_player.stop()
+        # on_song_finished() will be automatically called which will start the next song
+
+    def queue(self, song):
+        self.playlist = self.get_playlist()
+        self.playlist.append(song)
+        update_queue_file(self.playlist)
+
+    def clear_queue(self):
+        self.playlist = self.get_playlist()
+        del self.playlist[1:]
+        update_queue_file(self.playlist)
+
+    def remove(self, index):
+        self.playlist = self.get_playlist()
+        to_delete = self.playlist[index].file
+        del self.playlist[index]
+        update_queue_file(self.playlist)
+
+        executor = ProcessPoolExecutor(1)
+        del_future = self.bot.loop.run_in_executor(executor, remove_file, to_delete)
+        del_task = asyncio.ensure_future(del_future)
+        asyncio.ensure_future(del_task)
+
+    def file_needed(self, file):
+        self.playlist = self.get_playlist()
+        found = False
+        for index, song in enumerate(self.playlist):
+            if not index == 0:
+                if self.playlist[index].file == file:
+                    found = True
+        return found
+
+    def get_playlist(self):
+        return load_queue_file()
+
+    def run(self):
+        while self.is_alive:
+            self.play()
+            time.sleep(1)
+
+    def change_voice(self, vc):
+        self.voice_client = vc
+
+    def change_volume(self, volume):
+        self.volume = volume
+        self.media_player.volume = volume
+
+    def check_vars(self):
+        print(self.media_player.__dict__)
+        print(self.media_player)
+        print(inspect.getmembers(self.media_player, predicate=inspect.ismethod))
+
+    def stop(self):
+        self.media_player.stop()
+        self.clear_queue()
+        del self.media_player
+        self.is_alive = False
+
+# YT downloader stuff
+# ---------------------------------
+class YTDLogger(object):
+    def debug(self, msg):
+        print(msg)
+
+    def warning(self, msg):
+        print(msg)
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Finished downloading, converting ...')
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': True,
+    'audioformat': "mp3",
+    'outtmpl': '%(id)s',
+    'logger': YTDLogger(),
+    'progress_hooks': [my_hook],
+}
+
+
+def fetch_song(url):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, False)
+        if info_dict:
+            song = Song(
+                id=info_dict.get('display_id', None),
+                title=info_dict.get('title', None),
+                thumbnail=info_dict.get('thumbnail', None),
+                description=info_dict.get('description', None),
+                duration=info_dict.get('duration', None),
+                views=info_dict.get('view_count', None),
+                likes=info_dict.get('like_count', None),
+                dislikes=info_dict.get('dislike_count', None),
+                playlist=info_dict.get('playlist', None),
+                uploader=info_dict.get('uploader', None),
+                url=info_dict.get('webpage_url', None),
+                source="youtube.com",
+            )
+            return song
+        else:
+            return None
+
+
+def downloadSong(url):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
